f433be86824fec97e330913130602e963438145d
Maxim <veremeenkom91@gmail.com>
2023-10-28 19:23:48 +0500

    CgСписок порталов

--- src/views/admin-page/AdminPage.tsx
+++ src/views/admin-page/AdminPage.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Portal from "../portal/Portal";
-import BaseWrapper from "../../components/BaseWrapper";
-const AdminPage = () => {
+import BaseWrapper, {BaseWrapperSlot} from "../../components/BaseWrapper";
+import ActionButton, {ActionButtonSlot} from "../../components/ActionButton";
+import {MdModeEdit} from "react-icons/md";
+import {IoAdd} from "react-icons/io5";
+import {Button, FloatingLabel, Modal, Form} from "react-bootstrap";
+import MyModalConversion from "../page-left-menu/conversions-page/MyModalConversion";
+import ModalWindow from "../../myModal/ModalWindow";
+import RegistrPortal from "./RegistrPortal";
 
-   const defPortal = [
+const AdminPage = () => {
+   const [modalShow, setModalShow] = useState(false);
+   const [forms, setForms] = useState([
       {
          id: 1,
          title: "РЖД ОГУ",
@@ -22,14 +30,47 @@ const AdminPage = () => {
          nameHR: 'Пупкин К.В.',
          name: 'Основы РЖД'
       },
-   ]
-   return (
-      <div className={'app-container portal--container'}>
-         <h1 className={"mb-4"}>Порталы</h1>
+   ]);
 
-         {defPortal.map((value, index)=>
+
+
+
+   function onAddPortal(theme: any) {
+
+   }
+
+   function savePortal() {
+
+   }
+
+
+
+   return (
+      <div className={'app-container admin-page--container'}>
+         <BaseWrapper title="Порталы">
+            <BaseWrapperSlot>
+               <div className={'department-view--themes__theme--actions portal--button'}>
+                  <ActionButton text='Добавить портал' handler={() => setModalShow(true)}>
+                     <ActionButtonSlot>
+                        <IoAdd/>
+                     </ActionButtonSlot>
+                  </ActionButton>
+               </div>
+            </BaseWrapperSlot>
+         </BaseWrapper>
+
+         {forms.map((value, index) =>
             <Portal key={index} id={value.id} title={value.title} nameHR={value.nameHR} name={value.name}/>
          )}
+         {
+            !forms.length &&
+             <div>
+                 Список порталов пуст
+             </div>
+         }
+         <ModalWindow show={modalShow} onHide={() => setModalShow(false)}>
+            <RegistrPortal/>
+         </ModalWindow>
       </div>
    );
 };
--- src/views/portal/Portal.tsx
+++ src/views/portal/Portal.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import BaseWrapper, {BaseWrapperSlot} from "../../components/BaseWrapper";
 import {ListGroup} from "react-bootstrap";
+import ActionButton, {ActionButtonSlot} from "../../components/ActionButton";
+import {IoAdd} from "react-icons/io5";
+import {Navigate} from "react-router-dom";
+import {AiFillDelete, AiOutlineArrowRight} from "react-icons/ai";
+import {MdModeEdit} from "react-icons/md";
 
 const Portal = ({key, id, title, nameHR, name}) => {
+
+   function onEditPortal(id) {
+      
+   }
+
+   function onDeletePortal(id) {
+      
+   }
+
    return (
-      <div key={key} className={'w-100 mb-4 border-1'}>
+      <div key={key} className={'w-100 mb-4 border-1 portal--container'}>
          <BaseWrapper title={title} smallTitle={true}>
             <BaseWrapperSlot>
+               <div className="department-view--themes__theme--actions">
+                  <ActionButton text='Редактировать' handler={() => onEditPortal(id)}>
+                     <ActionButtonSlot>
+                        <MdModeEdit />
+                     </ActionButtonSlot>
+                  </ActionButton>
+                  <ActionButton text='Удалить' handler={() => onDeletePortal(id)}>
+                     <ActionButtonSlot>
+                        <AiFillDelete />
+                     </ActionButtonSlot>
+                  </ActionButton>
+               </div>
                <ListGroup as="ol">
                   <ListGroup.Item
                      as="li"
@@ -22,7 +48,13 @@ const Portal = ({key, id, title, nameHR, name}) => {
                            Имя создателя: <span>{nameHR}</span>
                         </p>
                         </div>
-
+                     </div>
+                     <div className={'my-auto'}>
+                        <ActionButton text='Перейти к порталу' handler={() => {<Navigate to={'./'}/>}}>
+                           <ActionButtonSlot>
+                              <AiOutlineArrowRight />
+                           </ActionButtonSlot>
+                        </ActionButton>
                      </div>
                   </ListGroup.Item>
                </ListGroup>
